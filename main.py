919e3a5e4e99114311c9eaf6eed0ae2d3e3c21d9
werinq <werinq.real@gmail.com>
2025-08-12 21:28:36 +0200

    im a failure

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    images_folder = os.path.join(app.static_folder, "images")
+    image_files = [
+        f"images/{img}"
+        for img in os.listdir(images_folder)
+        if img.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".webp"))
+    ]
+    return render_template("index.html", images=image_files)
+
+if __name__ == "__main__":
+    app.run(debug=True)
